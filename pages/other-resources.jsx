34cc34ae72a87aaf1cd48b958acdde1ec557cccf
outbreak-activities <always7snape@gmail.com>
2021-12-02 21:34:55 -0600

    sample changes to  other resources

--- pages/other-resources.jsx
+++ pages/other-resources.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
-import { Box, Button, Typography, useTheme, Paper } from "@material-ui/core";
+import { Box, Button, Typography, useTheme, Paper, Toolbar, IconButton } from "@material-ui/core";
 import { useRouter } from "next/router";
 
 import NavBar from "../components/nav/NavBar";
@@ -15,7 +15,17 @@ const Page = () => {
     <>
       <CssBaseline />
       <NavBar />
-      {/* Do stuff here */}
+      {/* Do stuf */
+      <AppBar position = "static"> 
+        <Toolbar variant = "dense">
+          <IconButton edge = "start" color = "inherit" aria-label = "menu" sx = {{ mr: 2 }}>
+            <MenuIcon />
+          </IconButton>
+          <Typography variant = "h6" color = "inherit" component = "div">
+            </Typography>        
+        </Toolbar>
+      </AppBar>
+      /* here */}
       {/* <BookmarkBorderIcon /> */}
     </>
   );
