4357834457fec0cefb3e590ca8b21009cdb4a2c6
Jay Reiter <jayr2@illinois.edu>
2022-01-06 20:27:10 -0600

    added zoom buttons (that dont work)

--- components/ResourceMap/ResourceMap.jsx
+++ components/ResourceMap/ResourceMap.jsx
@@ -1,6 +1,15 @@
 import React, { useState, useRef, useEffect } from "react";
-import { useTheme, Box } from "@material-ui/core";
+import {
+  useTheme,
+  Box,
+  Typography,
+  IconButton,
+  Button,
+} from "@material-ui/core";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
+
+import AddIcon from "@material-ui/icons/Add";
+import RemoveIcon from "@material-ui/icons/Remove";
 
 const ResourceMap = (props) => {
   const theme = useTheme();
@@ -34,20 +43,49 @@ const ResourceMap = (props) => {
       setLat(map.current.getCenter().lat.toFixed(4));
       setZoom(map.current.getZoom().toFixed(2));
     });
-  });
+  }, [lng, lat, zoom]);
 
   const [cursorStyle, setCursorStyle] = useState("grab");
 
   return (
-    <Box
-      style={{
-        cursor: cursorStyle,
-      }}
-      onMouseDown={() => setCursorStyle("grabbing")}
-      onMouseUp={() => setCursorStyle("grab")}
-    >
-      Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      <div ref={mapContainer} style={{ height: "80vh" }} />
+    <Box style={{ position: "relative" }}>
+      <Box style={{ position: "absolute", top: "8px", right: "8px" }}>
+        {/* Longitude: {lng} | Latitude: {lat} | Zoom: {zoom} */}
+        <IconButton
+          variant="contained"
+          onClick={() => setZoom(map.current.getZoom() + 1)}
+          style={{
+            padding: "4px",
+            marginRight: "8px",
+            backgroundColor: theme.palette.grey[300],
+            borderRadius: "8px",
+            boxShadow: "0 0 8px 4px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          <AddIcon />
+        </IconButton>
+        <IconButton
+          variant="contained"
+          onClick={() => setZoom(map.current.getZoom() - 1)}
+          style={{
+            padding: "4px",
+            backgroundColor: theme.palette.grey[300],
+            borderRadius: "8px",
+            boxShadow: "0 0 8px 4px rgba(0, 0, 0, 0.2)",
+          }}
+        >
+          <RemoveIcon />
+        </IconButton>
+      </Box>
+      <Box
+        style={{
+          cursor: cursorStyle,
+        }}
+        onMouseDown={() => setCursorStyle("grabbing")}
+        onMouseUp={() => setCursorStyle("grab")}
+      >
+        <div ref={mapContainer} style={{ height: "80vh" }} />
+      </Box>
     </Box>
   );
 };
